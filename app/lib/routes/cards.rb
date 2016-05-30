d02915ee3a050a30143f155977c5ca84cf9bd966
Sasha Cooper <jinksy@gmail.com>
2016-05-30 15:26:36 +0100

    Allow search to function with surrounding whitespace

--- app/lib/routes/cards.rb
+++ app/lib/routes/cards.rb
@@ -3,9 +3,11 @@ Pakyow.app.routes :cards do
     tag_contents = Tag.ids_and_full_names.map do |id, full_name|
       { tag: { value: id, content: full_name } }
     end
+
     view.scope(:'tag-names').apply(tag_contents)
 
-    name, tags = params["card-name"].to_s, params['tag-ids'].to_a
+    name = params["card-name"].to_s.strip
+    tags = params['tag-ids'].to_a
     cards = Card.by_name(name).tagged_with(tags)
 
     view.scope(:card).apply(cards) do |context, card|
